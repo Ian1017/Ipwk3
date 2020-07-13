3eb9a499c19b5c9d269bc6f65d52a1f68a7fda04
ian1017 <mdawidamengo@gmail.com>
2020-07-13 12:21:13 +0300

    create a view to handle displaying of random projects in the index page

--- myawwards/views.py
+++ myawwards/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+def index(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+    else:
+        form = PostForm()
+
+    try:
+        posts = Post.objects.all()
+        posts = posts[::-1]
+        a_post = random.randint(0, len(posts)-1)
+        random_post = posts[a_post]
+    except Post.DoesNotExist:
+        posts = None
+    return render(request, 'awwards/index.html', {'posts': posts, 'form': form, 'random_post': random_post})
+
+
 
 # Create your views here.
 def index(request):
