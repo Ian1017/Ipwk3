e557f00ac14e26877c59543884b7316b4641201e
ian1017 <mdawidamengo@gmail.com>
2020-07-14 01:46:25 +0300

    add procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn myawward.wsgi --log-file -
--- myawwards/models.py
+++ myawwards/models.py
@@ -19,15 +19,15 @@ class Profile(models.Model):
         return f"{self.user.username } Profile"
 
     @receiver(post_save,sender=User)
-    def create_user_profile(sender,instance,created,**kwargs):
+    def create_user_profile(sender, instance, created,**kwargs):
         if created:
             Profile.objects.create(user=instance)
 
     @receiver(post_save,sender=User)
-    def save_user_profile(sender,instance,**kwargs):
+    def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
 
-        
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=300)
