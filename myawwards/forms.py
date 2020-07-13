211f4f435d2cd6a3e95ac37ed46861070945775f
ian1017 <mdawidamengo@gmail.com>
2020-07-14 01:34:08 +0300

    add rating form

--- myawwards/forms.py
+++ myawwards/forms.py
@@ -27,3 +27,12 @@ class UpdateUserForm(forms.ModelForm):
 
 
 class UpdateUserProfileForm(forms.ModelForm):
+        class Meta:
+        model = Profile
+        fields = ['name', 'location', 'prof_picture', 'bio', 'contact']
+
+
+class RatingsForm(forms.ModelForm):
+    class Meta:
+        model = Rating
+        fields = ['design', 'usability', 'content']
