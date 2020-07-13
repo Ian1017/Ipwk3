0739b6188aefc4cea406f4defe3d7ee21580a420
ian1017 <mdawidamengo@gmail.com>
2020-07-14 01:30:38 +0300

    add a post form

--- myawwards/forms.py
+++ myawwards/forms.py
@@ -1,3 +1,9 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Post, Profile, Rating
+from pyuploadcare.dj.forms import ImageField
+
 class SignupForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
 
@@ -5,3 +11,9 @@ class SignupForm(UserCreationForm):
         model = User
         fields = ('username', 'email', 'password1', 'password2')
 
+class PostForm(forms.ModelForm):
+    photo = ImageField(label='')
+
+    class Meta:
+        model = Post
+        fields = ('photo', 'title', 'url', 'description', 'technology',)
