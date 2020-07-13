ec699f60224cd01162ac59a85aa2657fea0d0ea3
ian1017 <mdawidamengo@gmail.com>
2020-07-14 01:08:49 +0300

    import needed modules

--- myawwards/views.py
+++ myawwards/views.py
@@ -1,3 +1,13 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .forms import PostForm, UpdateUserForm, UpdateUserProfileForm, RatingsForm, SignupForm
+from .models import Post, Profile, Rating
+from rest_framework import viewsets
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+from .serializer import ProfileSerializer, UserSerializer, PostSerializer
+import random
 def index(request):
     if request.method == "POST":
         form = PostForm(request.POST)
