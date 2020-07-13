4c4c09b835b974126759cbcd0f5f2db4145db5ff
ian1017 <mdawidamengo@gmail.com>
2020-07-14 01:28:43 +0300

    add user sign up form

---
+++ myawwards/forms.py
@@ -0,0 +1,7 @@
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
