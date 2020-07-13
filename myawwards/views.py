de49370ed5b5e43992c6819a771656aa1ca7b33c
ian1017 <mdawidamengo@gmail.com>
2020-07-13 21:26:47 +0300

    create an edit profile function

--- myawwards/views.py
+++ myawwards/views.py
@@ -50,6 +50,33 @@ def signup(request):
 def profile(request, username):
     return render(request, 'profile.html')
 
+def user_profile(request,username):
+    user_prof = get_object_or_404(User,username= username)
+    if request.user == user_prof:
+        return redirect('profile',username=request.user.username)
+    context = {
+        'user-prof':user_prof
+    }    
+    return render(request,'userprofile.html',context)
+
+@login_required(login_url='login')
+def edit_profile(request, username):
+    user = User.objects.get(username=username)
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        prof_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and prof_form.is_valid():
+            user_form.save()
+            prof_form.save()
+            return redirect('profile', user.username)
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        prof_form = UpdateUserProfileForm(instance=request.user.profile)
+    params = {
+        'user_form': user_form,
+        'prof_form': prof_form
+    }
+    return render(request, 'edit.html', params)
 
 # Create your views here.
 
